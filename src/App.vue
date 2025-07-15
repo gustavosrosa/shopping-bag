5c6b4687eec867aa55e8496d2d67a107bda3a990
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-15 11:26:15 -0300

    SHBG: FEAT: Criada logica de carrinho vazio

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> -
+    <router-link to="/">Home</router-link><span> - </span>
     <router-link to="/basket">Shopping Bag ({{ this.productsInBag.length }})</router-link>
   </div>
   <router-view />
