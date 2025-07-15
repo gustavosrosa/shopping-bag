03c07745b28d102c201c3e7850d051278bf6325f
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-15 10:23:25 -0300

    SHBG: FEAT: Criada acao de load products

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,21 @@
+import axios from 'axios';
 import { createStore } from 'vuex'
 
 export default createStore({
+  // Propriedade de dados
   state: {
-    
+
   },
   mutations: {
-    
+
   },
   actions: {
+
+    loadProducts() {
+      axios.get("https://fakestoreapi.com/products").then((response) => {
+        console.log(response);
+      })
+    }
 
   },
   modules: {
